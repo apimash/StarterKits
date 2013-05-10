96583ecccbd9c912f5916e9dcfca1a8f75089360
Jim O'Neil <jim.oneil@live.com>
2013-05-10 14:54:15 -0400

    renaming of Debug folder to Diagnostics to avoid .gitignore issues

---
+++ APIMASH_BingMaps_StarterKit/APIMASHLib/Diagnostics/ApiDebugPanel.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+//
+// LICENSE: http://opensource.org/licenses/ms-pl
+//
+
+namespace APIMASH.Diagnostics
+{
+    public sealed partial class ApiDebugPanel : UserControl
+    {
+        public ApiDebugPanel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
