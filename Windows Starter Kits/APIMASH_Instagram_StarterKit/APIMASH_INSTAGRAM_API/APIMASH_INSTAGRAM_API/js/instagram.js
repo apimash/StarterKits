df67ae8219f866e0eea53050642fb520547f1647
Stacey <stacey.mulcahy@gmail.com>
2013-06-19 15:00:54 -0400

    Adding Instagram JS Starter Kit
    Basic use of instagram starter kit

---
+++ Windows Starter Kits/APIMASH_Instagram_StarterKit/APIMASH_INSTAGRAM_API/APIMASH_INSTAGRAM_API/js/instagram.js
@@ -0,0 +1,100 @@
+﻿
+var Instagram = function () {
+
+    // this will be a private property
+    var clientId = '';
+    var baseUrl = 'https://api.instagram.com/v1';
+    // this will be a public method
+    var init = function (client_Id) {
+        clientId = client_Id;
+    }
+
+    var endPoints={
+        'getRecentMediaByTag':'/tags/{tag}/media/recent',
+        'getPopularMedia':'/media/popular',
+        'getMediaInLonLat':'/media/search',
+        'getLocations':'/locations/search',
+        'getMediaByLocationId':'/locations/{location-id}/media/recent',
+        'searchTagsByName':'/tags/search'
+    }
+
+   
+    var getRecentMediaByTag = function (tag,options)
+    {
+        if (!options)
+        {
+            options = {};
+        }
+        var url = endPoints['getRecentMediaByTag']
+        var newUrl = url.replace('{tag}', tag);
+        return (makeJSONRequest(newUrl,options));
+    }
+
+   
+    var getPopularMedia = function ()
+    {
+        var url = endPoints['getPopularMedia']
+        return (makeJSONRequest(url));
+    }
+    
+
+    var getMediaInLonLat = function (options)
+    {
+        var url = endPoints['getMediaInLonLat']
+        var data = { 'lng': options.lng,'lat':options.lat };
+        return (makeJSONRequest(url, data));
+    }
+    var getLocations = function (options) {
+        // needs to be sorted
+        var url = endPoints['getLocations']
+        var data = { 'lng': options.lng, 'lat': options.lat };
+        return (makeJSONRequest(url,data));
+    }
+    var getMediaByLocationId = function (location_id, options) {
+        if (!options) {
+            options = {};
+        }
+        var url = endPoints['getMediaByLocationId']
+        var newUrl = url.replace('{location-id}', location_id);
+        return (makeJSONRequest(newUrl,options));
+    }
+    var searchTagsByName = function (name)
+    {
+        var url = endPoints['searchTagsByName']
+        var data={'q':name};
+        return(makeJSONRequest(url,data));
+    }
+
+    var getNextUrl = function (url)
+    {
+        var jqxhr = $.getJSON(url);
+        return (jqxhr);
+    }
+  
+    var makeJSONRequest = function (url,data)
+    {
+        var self = this;
+        if(!data){data={}}
+        var modUrl = baseUrl + url;
+        data.client_id = clientId;
+        var jqxhr = $.getJSON( modUrl,data);
+        return(jqxhr);
+       
+    }
+
+  
+    
+
+    return {
+        // public properties
+        init: init,
+        searchTagsByName: searchTagsByName,
+        getMediaInLonLat: getMediaInLonLat,
+        getPopularMedia: getPopularMedia,
+        getRecentMediaByTag: getRecentMediaByTag,
+        getLocations: getLocations,
+        getMediaByLocationId: getMediaByLocationId,
+        getNextUrl:getNextUrl
+        
+    }
+}();
---
+++ Windows Starter Kits/APIMASH_Instagram_StarterKit/APIMASH_INSTAGRAM_API/APIMASH_INSTAGRAM_API/pages/popular/popular.js
@@ -0,0 +1,128 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+    var publicMembers =
+     {
+         itemList: new WinJS.Binding.List(),
+         tags:[]
+     };
+    WinJS.Namespace.define("InstagramPopular", publicMembers);
+    
+    WinJS.UI.Pages.define("/pages/popular/popular.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        el:'',
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            Instagram.init(InstagramAPIGlobals.clientId);
+            if (InstagramPopular.tags.length)
+            {
+                this.processTags(InstagramPopular.tags);
+            }
+            this.el = $(element);
+            this.getPopularMedia();
+        },
+
+        getPopularMedia: function () {
+            
+            var self = this;
+            Instagram.getPopularMedia().done(function (data) {
+                self.processData(data);
+            })
+        },
+
+        processData: function (data)
+        {
+
+        
+            var tagsList = [];
+            var tagHash = {};
+            // loop through the items
+            var len = data.data.length;
+            var myList= new WinJS.Binding.List();
+            for (var i = 0; i < len; i++)
+            {
+                // Need to loop through tags
+               var item = data.data[i];
+               var tags = item.tags;
+               var tlen = tags.length;
+               for (var j = 0; j < tlen; j++)
+               {
+                   var label = tags[i];
+                   if (!tagHash[label])
+                   {
+                      tagHash[label] = true;
+                      if (label)
+                      {
+                         tagsList.push(label);
+                      }
+                   }
+               }
+                myList.push(item);
+              }
+             // create the tag list 
+            this.processTags(tagsList);
+      
+      
+            // force an update 
+            InstagramPopular.itemList = myList;
+            var list = document.getElementById('popularListView').winControl;
+            list.itemDataSource = myList.dataSource;
+            myList.notifyReload();
+            
+          
+    
+            
+          
+        },
+
+        getMediaByTag:function(tag)
+        {
+           
+            var self = this;
+            Instagram.getRecentMediaByTag(tag).done(function (data) {
+                self.processData(data);
+            });
+            
+        },
+
+        processTags: function (tags) {
+            InstagramPopular.tags = tags;
+            var self = this;
+            $(this.el).find('ul li').remove();
+            if (!tags.length) {
+                var div = "<li><a href='#' class='a-tag'>No tags found</a></li>"
+                $(this.el).find('ul').append(div);
+                return;
+            }
+            for (var i = 0; i < tags.length; i++)
+            {
+               
+                var div = "<li><a href='#' class='a-tag'>" + tags[i] + "</a></li>"
+                $(this.el).find('ul').append(div);
+              
+            }
+
+            $(this.el).find('ul li a').click(function (evt) {
+                evt.preventDefault();
+             
+                var tag = $(evt.target).text();
+                self.getMediaByTag(tag);
+
+            });
+
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+            InstagramPopular.itemList = new WinJS.Binding.List();
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
