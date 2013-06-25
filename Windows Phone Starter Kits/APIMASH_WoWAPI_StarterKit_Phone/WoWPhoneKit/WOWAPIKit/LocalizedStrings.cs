b0dc00ae65b7321f14a20484a4e028dfcecc5444
David Isbitski <david.isbitski@microsoft.com>
2013-06-25 15:22:08 -0400

    Initial release

---
+++ Windows Phone Starter Kits/APIMASH_WoWAPI_StarterKit_Phone/WoWPhoneKit/WOWAPIKit/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WOWAPIKit.Resources;
+
+namespace WOWAPIKit
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
